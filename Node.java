14b5ae3eb4e846a60e44de9fea1c72fb619cf069
Noah Sibai <sibai1nk@cmich.edu>
2016-04-25 18:39:04 -0400

    Added files via upload

--- Node.java
+++ Node.java
@@ -1,29 +1,54 @@
+package checkGroup;
+
+import java.awt.Point;
+
+/**
+ * @author Noah Sibai, Logan Greer, Paul Lamine, Bradley Davis, Zachary Casavant
+ * @fileName Node.java
+ * @version 1.0
+ */
+
 public class Node {
 
 	String message;
 	String choiceR;
 	String choiceL;
-	String pos;
+	Point pos;
 
+	public Node(String m, String one, String two, Point pos) {
+		if (one.equals("null"))
+			one = null;
+		if (two.equals("null"))
+			two = null;
 
-	Node lChild;
-	Node rChild;
-
-	public Node( String m, String one, String two, String pos)
-	{
 		this.message = m;
-		this.choiceR = one;
-		this.choiceL = two;
-		this.pos = pos;
-
-		lChild = null;
-		rChild = null;
-
+		this.choiceR = two;
+		this.choiceL = one;
+		this.pos = new Point(pos);
 	}
 
-
-	public String toString()
-	{
-		return(message+" "+ choiceR +" "+ choiceL + pos);
+	public Point getPos() {
+		return pos;
 	}
+
+	public String getButtonL() {
+		return choiceL;
+	}
+
+	public String getButtonR() {
+		return choiceR;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String toString() {
+		return (message + " " + choiceR + " " + choiceL + pos.toString());
+	}
+
+	public boolean equals(Point p) {
+		return this.pos.equals(p);
+	}
+
 }
